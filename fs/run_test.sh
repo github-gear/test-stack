f5a1b634d248c141ce514acb5003af343d03931c
Aaron Lim <aaron.lim.yu.kwang@gmail.com>
2016-05-08 16:30:30 +0800

    ok

---
+++ fs/run_test.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/github-gear/fake-fs.git /fs
+
+cd /fs
+git checkout "$BRANCH"
+gem install bundler && bundle install
+
+./dynamic_test # stands for rspec, exist to show pass test in master and fail test in staging
